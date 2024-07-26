1c1a6f6f6a4180683968a2bb64e8ef3ac77d3785
Anshika-Gupta05 <anshika.gupta2021@vitstudent.ac.in>
2024-07-26 20:13:37 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
-import LoginSignup from "./Pages/LoginSignup";
 function App() {
   return (
     <div>
@@ -20,7 +19,6 @@ function App() {
             <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
